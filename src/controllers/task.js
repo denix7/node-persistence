8ee578cc68b8fd595695ff8b1f9ab9d2b033d999
Denix7 <dennis.brian.pv@gmail.com>
2020-03-01 00:33:57 -0400

    updateTask and deleteTask  implemented

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -8,16 +8,17 @@ const getTasks = (req, res) => {
 
 const createTask = (req, res) => {
 
-    res.send('its works');
     const newTask = {
         id: v4(),
         name: req.body.name,
         description: req.body.description
     };
-
+    
     getConnection().get('tasks')
     .push(newTask)
     .write();
+
+    res.send('task created');
 }
 
 const getTask = (req, res) => {
@@ -30,8 +31,36 @@ const getTask = (req, res) => {
     res.json(task);
 }
 
+const updateTask = async (req, res) => {
+    id = req.params.id;
+
+    const result = await getConnection().get('tasks')
+    .find({id: id})
+    .assign(req.body)
+    .write();
+
+    res.json({
+        message: 'task updated successfully',
+        task: result
+    });
+}
+
+const deleteTask = (req, res) => {
+    id = req.params.id;
+
+    getConnection().get('tasks')
+    .remove({id: id})
+    .write()
+
+    res.json({
+        message: 'Deleted successfully'
+    });
+}
+
 module.exports = {
     getTasks,
     createTask,
-    getTask
+    getTask,
+    updateTask,
+    deleteTask
 }
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,7 +6,7 @@ const tasks = require('../controllers/task');
 router.get('/tasks', tasks.getTasks);
 router.get('/tasks/:id', tasks.getTask);
 router.post('/tasks', tasks.createTask);
-router.put('/tasks/:id', );
-router.delete('/tasks/:id', );
+router.put('/tasks/:id', tasks.updateTask);
+router.delete('/tasks/:id', tasks.deleteTask);
 
 module.exports = router;
